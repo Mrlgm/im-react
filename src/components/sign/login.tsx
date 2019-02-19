5a3a4472a54bcbb9dff470771e409cedbf87315e
Mrlgm <877205619@qq.com>
2019-02-19 14:04:22 +0800

    初始化文件目录

---
+++ src/components/sign/login.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.scss'
+
+class Login extends Component{
+  render(){
+    return <div className="hhh">
+      hhhh
+    </div>
+  }
+}
+
+export default Login
